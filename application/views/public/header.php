261c414a9eff3b2516fb47ca1459793953059627
honza <honza@Home1B>
2019-12-27 23:18:55 -0800

    Header

--- application/views/public/header.php
+++ application/views/public/header.php
@@ -89,7 +89,20 @@
             <button type="button" data-toggle="collapse" data-target="#navigation" class="navbar-toggler btn-template-outlined"><span class="sr-only">Toggle navigation</span><i class="fa fa-align-justify"></i></button>
             <div id="navigation" class="navbar-collapse collapse">
               <ul class="nav navbar-nav ml-auto">
-                <li class="nav-item dropdown active"><a href="javascript: void(0)" data-toggle="dropdown" class="dropdown-toggle">Home <b class="caret"></b></a>
+              <li class="nav-item active"><a href="#">Home </a></li>
+              <li class="nav-item"><a href="#">Events </a></li>
+              <li class="nav-item"><a href="#">Destinations </a></li>
+              <li class="nav-item"><a href="#">Blog </a></li>
+              <li class="nav-item"><a href="#">Jeep Garage </a></li>
+              <li class="nav-item dropdown"><a href="javascript: void(0)" data-toggle="dropdown" class="dropdown-toggle">
+                About <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li class="dropdown-item"><a href="index.html" class="nav-link">Our Club</a></li>
+                    <li class="dropdown-item"><a href="index2.html" class="nav-link">Contact</a></li>
+                    <li class="dropdown-item"><a href="index3.html" class="nav-link">Our Team</a></li>                    
+                </li>
+               <!-- <li class="nav-item dropdown active"><a href="javascript: void(0)" data-toggle="dropdown" class="dropdown-toggle">
+                Home <b class="caret"></b></a>
                   <ul class="dropdown-menu">
                     <li class="dropdown-item"><a href="index.html" class="nav-link">Option 1: Default Page</a></li>
                     <li class="dropdown-item"><a href="index2.html" class="nav-link">Option 2: Application</a></li>
@@ -168,9 +181,9 @@
                       </div>
                     </li>
                   </ul>
-                </li>
+                </li> --> 
                 <!-- ========== FULL WIDTH MEGAMENU ==================-->
-                <li class="nav-item dropdown menu-large"><a href="#" data-toggle="dropdown" data-hover="dropdown" data-delay="200" class="dropdown-toggle">All Pages <b class="caret"></b></a>
+               <!-- <li class="nav-item dropdown menu-large"><a href="#" data-toggle="dropdown" data-hover="dropdown" data-delay="200" class="dropdown-toggle">All Pages <b class="caret"></b></a>
                   <ul class="dropdown-menu megamenu">
                     <li>
                       <div class="row">
@@ -257,19 +270,11 @@
                           </ul>
                         </div>
                       </div>
-                    </li>
+                    </li> 
                   </ul>
-                </li>
+                </li> -->
                 <!-- ========== FULL WIDTH MEGAMENU END ==================-->
-                <!-- ========== Contact dropdown ==================-->
-                <li class="nav-item dropdown"><a href="javascript: void(0)" data-toggle="dropdown" class="dropdown-toggle">Contact <b class="caret"></b></a>
-                  <ul class="dropdown-menu">
-                    <li class="dropdown-item"><a href="contact.html" class="nav-link">Contact option 1</a></li>
-                    <li class="dropdown-item"><a href="contact2.html" class="nav-link">Contact option 2</a></li>
-                    <li class="dropdown-item"><a href="contact3.html" class="nav-link">Contact option 3</a></li>
-                  </ul>
-                </li>
-                <!-- ========== Contact dropdown end ==================-->
+                
               </ul>
             </div>
             <div id="search" class="collapse clearfix">
